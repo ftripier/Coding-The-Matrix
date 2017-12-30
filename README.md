a8bc2d07c2e584473f7c0ce3ad6307ccc5f59a99
Felix Tripier <ftripier@gmail.com>
2017-12-29 20:27:38 -0800

    inverted encoding matrix

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Coding The Matrix
 
 My notes and scratch work for Philip N. Klein's textbook Coding The Matrix
+
+# Running code
+
+`source felixenv.sh`
--- matrix/error_code_lab.py
+++ matrix/error_code_lab.py
@@ -1,31 +1,39 @@
 from matrix.matutil import listlist2mat
+from matrix.bitutil import mat2bits, bits2mat
 from vector.vecutil import list2vec
 from GF2 import zero, one
 
 G = listlist2mat([
-  [one, zero, one, one],
-  [one, one, zero, one],
-  [zero, zero, zero, one],
-  [one, one, one, zero],
-  [zero, zero, one, zero],
-  [zero, one, zero, zero],
-  [one, zero, zero, zero],  
+    [one, zero, one, one],
+    [one, one, zero, one],
+    [zero, zero, zero, one],
+    [one, one, one, zero],
+    [zero, zero, one, zero],
+    [zero, one, zero, zero],
+    [one, zero, zero, zero],  
 ])
 
 message1 = list2vec([one, zero, zero, one])
+encoded = G * message1
 
-print("the encoding of the message [1, 0, 0, 1] is ", G * message1)
+print("the encoding of the message [one, 0, 0, one] is ", encoded)
 
-twobytwo = listlist2mat([
-  [one, one],
-  [zero, one]
+def extract_original(encoded):
+  decoded = [encoded[6], encoded[5], encoded[4], encoded[2]]
+  return decoded
+
+print("\ndecoded %s" % extract_original(encoded))
+
+# we know that the kernel is trivial because G's row space spans GF(1)^4, and therefore that
+# G represents a one-to-one function, and therefore that it is invertible
+
+# heres the matrix dude!
+
+G_INVERSE = listlist2mat([
+    [zero, zero, zero, zero, zero, zero, one],
+    [zero, zero, zero, zero, zero, one, zero],
+    [zero, zero, zero, zero, one, zero, zero],
+    [zero, zero, one, zero, zero, zero, zero]
 ])
 
-inverse = listlist2mat([
-  [one, zero],
-  [one, zero]
-])
-
-print("GF(2) inverses", twobytwo * inverse)
-
-
+print("\ndecoded by inversion: %s" % (G_INVERSE * encoded))
