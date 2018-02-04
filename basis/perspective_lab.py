216a5390c8e5f3d3045da175a52bea9984a73ba4
Felix Tripier <ftripier@gmail.com>
2018-02-03 23:32:36 -0800

    add make equations

--- basis/perspective_lab.py
+++ basis/perspective_lab.py
@@ -11,3 +11,22 @@ def move2board(y):
     y['y2']/y['y3'],
     y['y3']/y['y3'],
   ])
+
+def make_equations(x1, x2, w1, w2):
+  u = vecutil.list2vec([
+    (w1 * x1),
+    (w1 * x2),
+    w1,
+    -x1,
+    -x2,
+    -1
+  ])
+  v = vecutil.list2vec([
+    (w2 * x1),
+    (w2 * x2),
+    w2,
+    -x1,
+    -x2,
+    -1
+  ])
+  return [u, v]
